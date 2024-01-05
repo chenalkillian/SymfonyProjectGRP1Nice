65ba1b430fe64d5fae03beb60f5aecb431cfa261
chenalkillian <killianc142@gmail.com>
2024-01-05 12:22:32 +0100

    Acccueil

--- src/Controller/GamesController.php
+++ src/Controller/GamesController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\GamesInfo;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Exception\NotSupported;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class GamesController extends AbstractController
 {
+
     #[Route('/games', name: 'app_games')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
-        return $this->render('games/index.html.twig', [
-            'controller_name' => 'GamesController',
-        ]);
+        $game=$entityManager->getRepository(GamesInfo::class)->findAll();
+        return $this->render('games/index.html.twig',['Game'=>$game]);
+
     }
 }
